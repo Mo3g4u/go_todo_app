e52de3a482dbe47bca1779a25e3b9567529dae5b
Mo3g4u <lax34volvic@gmail.com>
2022-08-04 14:04:11 +0900

    section-062

--- server_test.go
+++ server_test.go
@@ -11,8 +11,7 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-func TestRun(t *testing.T) {
-	t.Skip("リファクタリング中")
+func TestSurver_Run(t *testing.T) {
 
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
@@ -23,9 +22,14 @@ func TestRun(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
 
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	})
+
 	// 別ゴルーチンでテスト対象の「run」関数を実行してHTTPサーバーを起動する
 	eg.Go(func() error {
-		return run(ctx)
+		s := NewServer(l, mux)
+		return s.Run(ctx)
 	})
 
 	// エンドポイントに対してGETリクエストを送信する
